e1a25be2477c1e482ef4e4f1c503a41da1dafec0
redoneturzo <redoneprodhan141@gmail.com>
2026-06-29 11:13:41 +0600

    Add files via upload

---
+++ 06 - Insertion Sort/06.1.c
@@ -0,0 +1,28 @@
+///1.Write a C program to sort the array {1, 3, 1, 4, 5} in ascending order using Insertion Sort.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[] = {1, 3, 1, 4, 5};
+
+    for(int i = 1; i < 5; i++)
+    {
+        int temp = arr[i];
+        int j;
+
+        for(j = i - 1; j >= 0 && arr[j] > temp; j--)
+        {
+            arr[j + 1] = arr[j];
+        }
+
+        arr[j + 1] = temp;
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
